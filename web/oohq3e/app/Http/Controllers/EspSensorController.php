4b0112ef521a4644a1914f57bb9a7d62f0536f3a
OOHQ3E <gaboly15@gmail.com>
2023-03-11 20:10:50 +0100

    Adding Model & Controller  for the esp sensor

---
+++ web/oohq3e/app/Http/Controllers/EspSensorController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\espSensor;
+use Illuminate\Http\Request;
+
+class EspSensorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'room' =>  'required',
+                'temperature'      =>  'required',
+                'humidity' => 'required'
+            ]
+        );
+        $esp = new espSensor();
+        $esp->room = $request->room;
+        $esp->temperature = $request->temperature;
+        $esp->humidity = $request->humidity;
+        $esp->save();
+        return with("success");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\espSensor  $espSensor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(espSensor $espSensor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\espSensor  $espSensor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(espSensor $espSensor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\espSensor  $espSensor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, espSensor $espSensor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\espSensor  $espSensor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(espSensor $espSensor)
+    {
+        //
+    }
+}
