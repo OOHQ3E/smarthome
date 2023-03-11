56a867cc7463412279c8be3053f4b5cbf0a7b31d
OOHQ3E <84786014+OOHQ3E@users.noreply.github.com>
2023-03-11 16:08:02 +0100

    feat(esp) sending t/h data to raspberry's api
    - ESP now sends data to raspberry, which can process the data for later use
    - sends humidity and temperature every 10 seconds

--- sensor/temp with esp32s/temperature measuring/temperature measuring.ino
+++ sensor/temp with esp32s/temperature measuring/temperature measuring.ino
@@ -1,31 +1,69 @@
 #include "DHT.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "raspberrySmarthome";
+const char* password = "huu1vi9doL";
+
+IPAddress local_IP(192,168,200,5);
+IPAddress gateway(192,168,200,1);
+IPAddress subnet(255,255,255,0);
 
 #define DHTPIN 23
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
+const char *host = "http://192.168.200.1/api";   // IP/web server address
+
 void setup() {
   Serial.begin(9600); 
   dht.begin();
+  delay(1000);
+
+  if(!WiFi.config(local_IP,gateway,subnet)){
+    Serial.println("STA failed to configure");
+  }
+
+  WiFi.mode(WIFI_OFF);        
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(ssid,password);
+  Serial.println("\nConnecting");
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
 }
 
 void loop() {
-  delay(2000);
-
+  //Declare object of class HTTPClient
+  HTTPClient http;
+  delay(1000);
+  //Prepare data
+  String temperature, humidity, postData;
   float h = dht.readHumidity();
   float t = dht.readTemperature();
+
+  humidity = String(h);
+  temperature = String(t);
+  
   
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  Serial.print("Humidity: "); 
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: "); 
-  Serial.print(t);
-  Serial.println(" *C ");
-
+  postData = "temperature=" + temperature + "&humidity=" + humidity ;
+  http.begin(host);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpCode = http.POST(postData);
+  String payload = http.getString();
+  //Serial.println(postData);
+  Serial.println(httpCode);
+  Serial.println(payload);
+  http.end();
+  delay(9000);
 }
