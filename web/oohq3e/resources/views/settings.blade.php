5485cd930f02feea28a52a3b218dc24eacf6e5d4
OOHQ3E <gaboly15@gmail.com>
2023-03-18 21:13:17 +0100

    edit(web) panels are now in flex-wrap

--- web/oohq3e/resources/views/settings.blade.php
+++ web/oohq3e/resources/views/settings.blade.php
@@ -13,9 +13,9 @@
         @endif
 
 
-        <div class="p-5 text-center m-auto w-3/4 flex">
+        <div class="p-5 text-center m-auto w-3/4 flex gap-2">
             <a href="{{ asset('/') }}" >
-                <button type="button" class="m-2 text-left w-16 h-16 bg-gray-400 rounded-full text-center  py-3 px-3  hover:bg-gray-500">
+                <button type="button" class="font-black text-left w-16 h-16 bg-gray-400 rounded-full text-center  py-3 px-3  hover:bg-gray-500">
                     ←
                 </button>
             </a>
@@ -24,71 +24,69 @@
                     Add new room
                 </div>
             </a>
-
         </div>
 
-        <div class="flex justify-center">
-            <div class="xl:w-3/4 p-5 sm:w-11/12">
+        <div class="">
+            <div class="p-3 lg:max-w-2/3 md:max-w-10/12 sm:max-w-11/12 flex flex-wrap justify-center">
                 @forelse ($rooms as $room)
-                    <h2 class=" text-3xl font-light text-left text-gray-100 py-3">{{$room->name}}</h2><br>
+                    <div><p class="text-left text-2xl font-light text-gray-100 py-3">{{$room->name}}</p>
+                        <div class="bg-gray-100 rounded-lg overflow-auto m-3 bg-opacity-90">
 
-                    <div class="bg-gray-100 rounded-lg overflow-auto m-3 bg-opacity-90">
-                       <div class="flex flex-wrap gap-4 justify-center m-5">
-                           <a href="/device/{{ asset('create') }}">
-                               <div class="my-1 bg-green-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-green-400 hover:text-white w-60 text-center">
-                                   Add device
-                               </div>
-                           </a>
-                           <a href="/settings/{{ $room -> id }}/edit">
-                               <div class="my-1 bg-yellow-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-yellow-400 hover:text-white w-60 text-center">
-                                   Modify room
-                               </div>
-                           </a>
-                           <form action="/delete" method="POST">
-                               @csrf<button class="uppercase my-1 uppercase bg-red-500 text-white rounded-full py-3 px-6 transition hover:bg-red-400 hover:text-white w-60 text-center" name="id" type="submit" value="{{ $room -> id }}">Delete room</button>
-                           </form>
-                       </div>
+                            <div class="flex flex-wrap gap-2 justify-center m-5">
+                                <a href="/device/{{ asset('create') }}">
+                                    <div class="my-1 bg-green-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-green-400 hover:text-white w-48 text-center">
+                                        Add device
+                                    </div>
+                                </a>
+                                <a href="/settings/{{ $room -> id }}/edit">
+                                    <div class="my-1 bg-yellow-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-yellow-400 hover:text-white w-48 text-center">
+                                        Modify room
+                                    </div>
+                                </a>
+                                <form action="/delete" method="POST">
+                                    @csrf<button class="uppercase my-1 uppercase bg-red-500 text-white rounded-full py-3 px-6 transition hover:bg-red-400 hover:text-white w-48 text-center" name="id" type="submit" value="{{ $room -> id }}">Delete room</button>
+                                </form>
+                            </div>
 
                             <!--------------------------->
-                        @forelse ($esps as $esp)
-                            @if($esp->room_id === $room-> id)
-                                <table class="m-5 p-2 bg-gray-100 hover:bg-gray-200 bg-opacity-75 hover:bg-opacity-95 rounded-lg">
-                                    <thead class="text-center">
+                            @forelse ($esps as $esp)
+                                @if($esp->room_id === $room-> id)
+                                    <table class="m-5 p-2 bg-gray-100 hover:bg-gray-200 bg-opacity-75 hover:bg-opacity-95 rounded-lg">
+                                        <thead class="text-center">
                                         <td class="w-5/6"></td>
                                         <td class="w-min"></td>
                                         <td class="w-min"></td>
-                                    </thead>
+                                        </thead>
 
-                                    <tbody>
-                                            <tr>
-                                                <td class="p-3 m-1/3">
-                                                    <ul class="m-1">
-                                                        <li><span class="font-bold">Name:</span> {{ $esp->name }} </li>
-                                                        <br>
-                                                        <li><span class="font-bold">Type:</span> {{ $esp->type }} </li>
-                                                        <br>
-                                                        <li><span class="font-bold">IP:</span> 192.168.200.<span class="font-semibold">{{ $esp->ip_End }}</span></li>
-                                                    </ul>
-                                                </td>
+                                        <tbody>
+                                        <tr>
+                                            <td class="p-3 m-1/3">
+                                                <ul class="m-1">
+                                                    <li><span class="font-bold">Name:</span> {{ $esp->name }} </li>
+                                                    <br>
+                                                    <li><span class="font-bold">Type:</span> {{ $esp->type }} </li>
+                                                    <br>
+                                                    <li><span class="font-bold">IP:</span> 192.168.200.<span class="font-semibold">{{ $esp->ip_End }}</span></li>
+                                                </ul>
+                                            </td>
 
-                                                <td class="p-2 m-1/3">
-                                                    <a href="/settings/{{ $esp -> id }}/edit">
-                                                        <p class=" uppercase p-2 text-center rounded-lg bg-yellow-300 transition hover:bg-yellow-400"> Modify device</p>
-                                                    </a>
-                                                <td class="p-2 m-1/3 text-white">
-                                                    <form action="/delete" method="POST">
-                                                        @csrf
-                                                        <button class=" p-2 uppercase text-center rounded-lg bg-red-600 transition hover:bg-red-800"   name="id" type="submit" value="{{ $esp -> id }}">Delete device</button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                </tbody>
-                            </table>
-                                        @endif
-                        @empty
-                        @endforelse
-
-                    </div>
+                                            <td class="p-2 m-1/3">
+                                                <a href="/settings/{{ $esp -> id }}/edit">
+                                                    <p class=" uppercase p-2 text-center rounded-lg bg-yellow-300 transition hover:bg-yellow-400"> Modify device</p>
+                                                </a>
+                                            <td class="p-2 m-1/3 text-white">
+                                                <form action="/delete" method="POST">
+                                                    @csrf
+                                                    <button class=" p-2 uppercase text-center rounded-lg bg-red-600 transition hover:bg-red-800"   name="id" type="submit" value="{{ $esp -> id }}">Delete device</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                @endif
+                            @empty
+                            @endforelse
+                        </div></div>
                         @empty
 
                             <div class="flex justify-center font-sans font-bold text-2xl text-black">
