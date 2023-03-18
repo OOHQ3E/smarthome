7d19877f7dfaaa957da2da2de54ba4912826466b
OOHQ3E <gaboly15@gmail.com>
2023-03-18 21:55:11 +0100

    grid layout for panels in settings

--- web/oohq3e/resources/views/settings.blade.php
+++ web/oohq3e/resources/views/settings.blade.php
@@ -26,25 +26,25 @@
             </a>
         </div>
 
-        <div class="">
-            <div class="p-3 lg:max-w-2/3 md:max-w-10/12 sm:max-w-11/12 flex flex-wrap justify-center">
+        <div class="p-2">
+            <div class="p-3 lg:max-w-3/12 md:max-w-10/12 sm:max-w-11/12 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 inline-block">
                 @forelse ($rooms as $room)
                     <div><p class="text-left text-2xl font-light text-gray-100 py-3">{{$room->name}}</p>
                         <div class="bg-gray-100 rounded-lg overflow-auto m-3 bg-opacity-90">
 
                             <div class="flex flex-wrap gap-2 justify-center m-5">
                                 <a href="/device/{{ asset('create') }}">
-                                    <div class="my-1 bg-green-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-green-400 hover:text-white w-48 text-center">
+                                    <div class="my-1 bg-green-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-green-400 hover:text-white w-64 text-center">
                                         Add device
                                     </div>
                                 </a>
                                 <a href="/settings/{{ $room -> id }}/edit">
-                                    <div class="my-1 bg-yellow-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-yellow-400 hover:text-white w-48 text-center">
+                                    <div class="my-1 bg-yellow-500 text-white uppercase rounded-full py-3 px-6 transition hover:bg-yellow-400 hover:text-white w-64 text-center">
                                         Modify room
                                     </div>
                                 </a>
                                 <form action="/delete" method="POST">
-                                    @csrf<button class="uppercase my-1 uppercase bg-red-500 text-white rounded-full py-3 px-6 transition hover:bg-red-400 hover:text-white w-48 text-center" name="id" type="submit" value="{{ $room -> id }}">Delete room</button>
+                                    @csrf<button class="uppercase my-1 uppercase bg-red-500 text-white rounded-full py-3 px-6 transition hover:bg-red-400 hover:text-white w-64 text-center" name="id" type="submit" value="{{ $room -> id }}">Delete room</button>
                                 </form>
                             </div>
 
@@ -54,19 +54,19 @@
                                     <table class="m-5 p-2 bg-gray-100 hover:bg-gray-200 bg-opacity-75 hover:bg-opacity-95 rounded-lg">
                                         <thead class="text-center">
                                         <td class="w-5/6"></td>
-                                        <td class="w-min"></td>
-                                        <td class="w-min"></td>
+                                        <td class="w-1/3"></td>
+                                        <td class="w-1/3"></td>
                                         </thead>
 
                                         <tbody>
                                         <tr>
                                             <td class="p-3 m-1/3">
                                                 <ul class="m-1">
-                                                    <li><span class="font-bold">Name:</span> {{ $esp->name }} </li>
+                                                    <li class="leading-normal"><span class=" font-bold">Name:</span> {{ $esp->name }} </li>
                                                     <br>
-                                                    <li><span class="font-bold">Type:</span> {{ $esp->type }} </li>
+                                                    <li class="leading-normal"><span class="font-bold">Type:</span> {{ $esp->type }} </li>
                                                     <br>
-                                                    <li><span class="font-bold">IP:</span> 192.168.200.<span class="font-semibold">{{ $esp->ip_End }}</span></li>
+                                                    <li class="leading-normal"><span class="font-bold">IP:</span> 192.168.200.<span class="font-semibold">{{ $esp->ip_End }}</span></li>
                                                 </ul>
                                             </td>
 
