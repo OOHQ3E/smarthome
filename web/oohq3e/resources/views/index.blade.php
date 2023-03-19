3c3f9a76e6ae57a3f470dfb3026879b80da6ea3b
OOHQ3E <gaboly15@gmail.com>
2023-03-19 01:01:17 +0100

    main page has grids and a settings button +stuff
    - buttons wider
    - temp data bigger

--- web/oohq3e/resources/views/index.blade.php
+++ web/oohq3e/resources/views/index.blade.php
@@ -3,23 +3,31 @@
 @section('title') {{'Index Page'}} @endsection
 
 @section('content')
+    <a class="rounded-full" href="{{ asset('settings') }}" >
+        <div class="text-3xl text-center w-16 h-16 bg-gray-400 rounded-full text-center m-3 p-3 hover:bg-gray-500">
+            &#9881;&#65039;
+        </div>
+    </a>
+   <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
 
     @forelse($rooms as $room)
-        <div class="bg-white bg-opacity-75 shadow-2xl p-4 rounded-lg m-auto lg:w-3/4 sm:w-10/12 my-4">
+        <div class="bg-white bg-opacity-75 shadow-2xl p-4 rounded-lg m-auto w-10/12 my-4">
             <p class="text-left font-bold m-2 text-xl">{{$room->name}}</p>
             <div class="flex flex-wrap gap-4 justify-center">
                 @forelse($esps as $esp)
                     @if($esp-> room_id === $room->id && $esp->type === "Sensor")
-                        <a href="{{ asset('chart/'.$esp->room_id) }}">
-                            <div class="bg-opacity-75 bg-gray-400 hover:bg-gray-500 p-4 rounded-lg">
-                                <p id="espData-{{$esp->room_id}}" class="">
-                                    <!--Temperature: 30°C<br> Humidity: 70% -->
-                                </p>
-                            </div>
-                        </a>
+                        <div class="w-full justify-center">
+                            <a href="{{ asset('chart/'.$esp->room_id) }}">
+                                <div class="h-24 bg-opacity-75 bg-gray-400 hover:bg-gray-500 p-4 rounded-lg">
+                                    <p id="espData-{{$esp->room_id}}" class="">
+                                        <!--Temperature: 30°C<br> Humidity: 70% -->
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
 
                     @elseif($esp-> room_id === $room->id && $esp->type === "Toggle")
-                        <div class="bg-opacity-75 bg-gray-400 p-4 rounded-lg">
+                        <div class="w-full text-center bg-opacity-75 bg-gray-400 p-4 rounded-lg">
                             <label for="toggle-{{$esp->ip_End}}" class="inline-flex relative items-center cursor-pointer">
                                 <input type="checkbox" value="" id="toggle-{{$esp->ip_End}}" class="sr-only peer" onclick="toggle({{$esp->ip_End}})">
                                 <div class="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
@@ -38,7 +46,7 @@
             <p class="text-left font-bold m-2 text-xl">There are no rooms added to the database!</p>
         </div>
     @endforelse
-
+   </div>
 
  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
  <script>
