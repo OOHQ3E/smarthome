69f93390c488354487ecef09815e88218bbc0595
root <gaboly15@gmail.com>
2023-03-19 01:17:53 +0100

    design(index.blade.php) updated
    - text bigger, some text made bolder

--- web/oohq3e/resources/views/index.blade.php
+++ web/oohq3e/resources/views/index.blade.php
@@ -18,7 +18,7 @@
             <div class="flex flex-wrap gap-4 justify-center">
                 @forelse($esps as $esp)
                     @if($esp-> room_id === $room->id && $esp->type === "Sensor")
-                        <div class="w-full justify-center">
+                        <div class="w-full text-xl justify-center">
                             <a href="{{ asset('chart/'.$esp->room_id) }}">
                                 <div class="h-24 bg-opacity-75 bg-gray-400 hover:bg-gray-500 p-4 rounded-lg">
                                     <p id="espData-{{$esp->room_id}}" class="">
@@ -33,7 +33,7 @@
                             <label for="toggle-{{$esp->ip_End}}" class="inline-flex relative items-center cursor-pointer">
                                 <input type="checkbox" value="" id="toggle-{{$esp->ip_End}}" class="sr-only peer" onclick="toggle({{$esp->ip_End}})">
                                 <div class="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                <span id="device-{{$esp->ip_End}}-span" class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-800"></span>
+                                <span id="device-{{$esp->ip_End}}-span" class="ml-3 text-lg font-medium text-gray-900 dark:text-gray-800"></span>
                             </label>
                         </div>
                     @endif
@@ -57,10 +57,10 @@
         setInterval(getLatest.bind('room',1), 10000)
         function getLatest(room){
             $.getJSON('http://192.168.200.1/esp/getLatest/'+room, function(data) {
-                var text = `Temperature: ${data.temperature}°C<br> Humidity: ${data.humidity}%`
+                var text = `<span class="font-semibold">Temperature:</span> ${data.temperature}°C<br> <span class="font-semibold">Humidity:</span> ${data.humidity}%`
                     document.getElementById("espData-"+room).innerHTML = text;
 		}).fail(function (){
-                document.getElementById("espData-"+room).innerHTML = "Data currently unavailable";
+                document.getElementById("espData-"+room).innerHTML = "Data currently unavailable from {{$esp->name}}";
             });
 
        }
@@ -79,11 +79,11 @@
                    state = "off";
                    document.getElementById("toggle-"+esp).checked = false;
                }
-               document.getElementById("device-"+esp+"-span").innerHTML = "LED is: "+state;
+               document.getElementById("device-"+esp+"-span").innerHTML = "{{$esp->name}} is: <span class='font-bold'>"+state+"</span>";
                //console.log(text);
 
            }).fail(function(){
-                   document.getElementById("device-"+esp+"-span").innerHTML = "LED is currently unavailable";
+                   document.getElementById("device-"+esp+"-span").innerHTML = "{{$esp->name}} is currently unavailable";
                     document.getElementById("toggle-"+esp).checked = false;
                    document.getElementById("toggle-"+esp).disabled = true;
            });
@@ -100,7 +100,7 @@
                 //console.log(resp);
                 getStatusOfLed(esp);
             }).fail(function(){
-                document.getElementById("device-"+esp+"-span").innerHTML = "LED is currently unavailable";
+                document.getElementById("device-"+esp+"-span").innerHTML = "{{$esp->name}} is currently unavailable";
                 document.getElementById("toggle-"+esp).checked = false;
                 document.getElementById("toggle-"+esp).disabled = true;
             });
